af78ea9e960a0e9f18870f39c14e9c981d0391fc
Daisy Zhuo <candydaisy12321@gmail.com>
2016-11-20 11:29:39 -0600

    add everything

---
+++ Assets/Scripts/changescene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Order());
+	}
+
+    IEnumerator Order()
+    {
+        yield return new WaitForSeconds(4.0f);
+        SceneManager.LoadScene(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
